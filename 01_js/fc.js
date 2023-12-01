65a17d05c8a1a19dc206447b92d52a3982521c49
Sergio Caballero <sergiocaballero1304@gmail.com>
2023-11-30 22:00:35 -0300

    probando

---
+++ 01_js/fc.js
@@ -0,0 +1,8 @@
+class Persona {
+    constructor(nombre, apellido, edad, ciudad) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.ciudad = ciudad
+    }
+}
